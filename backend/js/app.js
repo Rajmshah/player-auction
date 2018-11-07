588f25ab3ad3fa1b1b85321e49c96c5a6c4a7d83
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2018-11-07 17:56:11 +0530

    initialised all pages

--- backend/js/app.js
+++ backend/js/app.js
@@ -86,8 +86,111 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locat
             url: "/jagz",
             templateUrl: "views/jagz.html",
             controller: 'JagzCtrl'
-        });
+        })
 
+
+        //ACTUAL PAGES
+        .state('auction', {
+            url: "/auction",
+            templateUrl: "views/template.html",
+            controller: 'AuctionCtrl'
+        })
+        //home page
+        .state('homepage', {
+            url: "/homepage",
+            templateUrl: "views/template.html",
+            controller: 'HomePageTableCtrl'
+        })
+        //detail home page
+        .state('detailhomepage', {
+            url: "/detailhomepage/:id",
+            templateUrl: "views/template.html",
+            controller: 'HomePageDetailsCtrl'
+        })
+        //Team List
+        .state('teamlist', {
+            url: "/teamlist",
+            templateUrl: "views/template.html",
+            controller: 'TeamListTableCtrl'
+        })
+        //detail Team List
+        .state('detailteamlist', {
+            url: "/detailteamlist/:id",
+            templateUrl: "views/template.html",
+            controller: 'TeamListDetailsCtrl'
+        })
+        //Team Player List
+        .state('teamplayerlist', {
+            url: "/teamplayerlist",
+            templateUrl: "views/template.html",
+            controller: 'TeamPlayerListTableCtrl'
+        })
+        //detail Team Player List
+        .state('detailteamplayerlist', {
+            url: "/detailteamplayerlist/:id",
+            templateUrl: "views/template.html",
+            controller: 'TeamPlayerListDetailsCtrl'
+        })
+        //Team Details
+        .state('teamdetails', {
+            url: "/teamdetails",
+            templateUrl: "views/template.html",
+            controller: 'TeamDetailsTableCtrl'
+        })
+        //Team Details
+        .state('detailteamdetails', {
+            url: "/detailteamdetails/:id",
+            templateUrl: "views/template.html",
+            controller: 'TeamDetailsCtrl'
+        })
+        //category List
+        .state('categorylist', {
+            url: "/categorylist",
+            templateUrl: "views/template.html",
+            controller: 'CategoryListTableCtrl'
+        })
+        //detail category List
+        .state('detailcategorylist', {
+            url: "/detailcategorylist/:id",
+            templateUrl: "views/template.html",
+            controller: 'CategoryListDetailsCtrl'
+        })
+        //category Details
+        .state('categorydetails', {
+            url: "/categorydetails",
+            templateUrl: "views/template.html",
+            controller: 'CategoryDetailsTableCtrl'
+        })
+        //detail category Details
+        .state('detailcategorydetails', {
+            url: "/detailcategorydetails/:id",
+            templateUrl: "views/template.html",
+            controller: 'CategoryDetailsCtrl'
+        })
+        //Minimum Base Value
+        .state('basevalue', {
+            url: "/basevalue",
+            templateUrl: "views/template.html",
+            controller: 'MinimumBaseValueTableCtrl'
+        })
+        //detail category Details
+        .state('detailminimumbasevalue', {
+            url: "/detailminimumbasevalue/:id",
+            templateUrl: "views/template.html",
+            controller: 'MinimumBaseValueDetailsCtrl'
+        })
+        //sponsor
+        .state('sponsor', {
+            url: "/sponsor",
+            templateUrl: "views/template.html",
+            controller: 'SponsorTableCtrl'
+        })
+        //detail sponsor
+        .state('detailsponsor', {
+            url: "/detailsponsor/:id",
+            templateUrl: "views/template.html",
+            controller: 'SponsorDetailsCtrl'
+        });
     $urlRouterProvider.otherwise("/dashboard");
     $locationProvider.html5Mode(isproduction);
 });
