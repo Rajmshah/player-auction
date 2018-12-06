046fe696a81b9df942cd3d56832c2824718ffd57
sohan1 <shn619@gmail.com>
2018-12-06 22:58:41 +0530

    git - a git

--- frontend/js/controllers/about.js
+++ frontend/js/controllers/about.js
@@ -1,5 +1,13 @@
-myApp.controller('AboutCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
-    $scope.template = TemplateService.getHTML("content/about.html");
-    TemplateService.title = "About Us"; // This is the Title of the Website
-    $scope.navigation = NavigationService.getNavigation();
+myApp.controller("AboutCtrl", function(
+  $scope,
+  TemplateService,
+  NavigationService,
+  $timeout,
+  toastr,
+  $http
+) {
+  $scope.template = TemplateService.getHTML("content/about.html");
+  TemplateService.title = "About Us"; // This is the Title of the Website
+  $scope.navigation = NavigationService.getNavigation();
+  $scope.adminurl = adminurl;
 });
--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,78 +1,96 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService;
-    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        $(window).scrollTop(0);
+myApp.controller("headerCtrl", function(
+  $scope,
+  TemplateService,
+  NavigationService
+) {
+  $scope.template = TemplateService;
+  $scope.$on("$stateChangeSuccess", function(
+    event,
+    toState,
+    toParams,
+    fromState,
+    fromParams
+  ) {
+    $(window).scrollTop(0);
+  });
+  $.fancybox.close(true);
+  var teamSubtitle = [];
+  var categorySubtitle = [];
+  $scope.adminurl = adminurl;
+  // GET Team
+  $scope.getTeam = function() {
+    $scope.url = "Teamlist/getList";
+    $scope.constraints = {};
+    NavigationService.apiCall($scope.url, $scope.constraints, function(data) {
+      _.each(data.data, function(n) {
+        teamSubtitle.push({
+          name: n.name,
+          classis: "active",
+          stateName: "teamdetail",
+          id: n._id
+        });
+      });
     });
-    $.fancybox.close(true);
-    var teamSubtitle = [];
-    var categorySubtitle = [];
-    // GET Team 
-    $scope.getTeam = function () {
-        $scope.url = 'Teamlist/getList';
-        $scope.constraints = {};
-        NavigationService.apiCall($scope.url, $scope.constraints, function (data) {
-            _.each(data.data, function (n) {
-                teamSubtitle.push({
-                    'name': n.name,
-                    'classis': "active",
-                    'stateName': "teamdetail",
-                    'id': n._id
-                })
-            })
-        });
-    }
-    $scope.getTeam();
-    // GET  Team END
+  };
+  $scope.getTeam();
+  // GET  Team END
 
-    // GET Category 
-    $scope.getCategory = function () {
-        $scope.url = 'Categorylist/getList';
-        $scope.constraints = {};
-        NavigationService.apiCall($scope.url, $scope.constraints, function (data) {
-            _.each(data.data, function (n) {
-                categorySubtitle.push({
-                    'name': n.name,
-                    'classis': "active",
-                    'stateName': "categorydetail",
-                    'id': n._id
-                })
-            })
+  // GET Category
+  $scope.getCategory = function() {
+    $scope.url = "Categorylist/getList";
+    $scope.constraints = {};
+    NavigationService.apiCall($scope.url, $scope.constraints, function(data) {
+      _.each(data.data, function(n) {
+        categorySubtitle.push({
+          name: n.name,
+          classis: "active",
+          stateName: "categorydetail",
+          id: n._id
         });
-    }
-    $scope.getCategory();
-    // GET  Category END
+      });
+    });
+  };
+  $scope.getCategory();
+  // GET  Category END
 
-    if (_.isEmpty(teamSubtitle) && _.isEmpty(categorySubtitle)) {
-        $scope.headerMenu = [{
-            name: "Auction Stats",
-            classis: "active",
-            anchor: 'auction',
-            subnav: teamSubtitle
-        }, {
-            name: "Groups",
-            classis: "active",
-            anchor: "auction",
-            subnav: categorySubtitle
-        }, {
-            name: "Sold Player",
-            classis: "active",
-            anchor: "soldplayers",
-            subnav: []
-        }, {
-            name: "Sponsor",
-            classis: "active",
-            anchor: "sponsor",
-            subnav: []
-        }, {
-             name: "About Us",
-             classis: "active",
-             anchor: "about",
-             subnav: []
-         }, {
-            name: "Live Stats",
-            classis: "active",
-            anchor: "livestats",
-            subnav: []
-        }];
-    }
+  if (_.isEmpty(teamSubtitle) && _.isEmpty(categorySubtitle)) {
+    $scope.headerMenu = [
+      {
+        name: "Auction Stats",
+        classis: "active",
+        anchor: "auction",
+        subnav: teamSubtitle
+      },
+      {
+        name: "Groups",
+        classis: "active",
+        anchor: "auction",
+        subnav: categorySubtitle
+      },
+      {
+        name: "Sold Player",
+        classis: "active",
+        anchor: "soldplayers",
+        subnav: []
+      },
+      {
+        name: "Sponsor",
+        classis: "active",
+        anchor: "sponsor",
+        subnav: []
+      },
+      {
+        name: "About Us",
+        classis: "active",
+        anchor: "about",
+        subnav: []
+      },
+      {
+        name: "Live Stats",
+        classis: "active",
+        anchor: "livestats",
+        subnav: []
+      }
+    ];
+  }
 });
