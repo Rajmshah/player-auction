c24c7c54ce7048ff68985665ee7d7486a98514c1
sohan1 <shn619@gmail.com>
2018-12-09 00:15:15 +0530

    sponsors

--- frontend/js/controllers/sponsor.js
+++ frontend/js/controllers/sponsor.js
@@ -1,5 +1,13 @@
-myApp.controller('SponsorCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
-    $scope.template = TemplateService.getHTML("content/sponsor.html");
-    TemplateService.title = "Sponsors"; // This is the Title of the Website
-    $scope.navigation = NavigationService.getNavigation();
+myApp.controller("SponsorCtrl", function(
+  $scope,
+  TemplateService,
+  NavigationService,
+  $timeout,
+  toastr,
+  $http
+) {
+  $scope.template = TemplateService.getHTML("content/sponsor.html");
+  TemplateService.title = "Sponsors"; // This is the Title of the Website
+  $scope.navigation = NavigationService.getNavigation();
+  $scope.adminurl = adminurl;
 });
