bc17e73fcf70ac5bece6bc390ea412834ccc996f
Rajmshah <rajwohlig@gmail.com>
2019-02-22 23:44:08 +0530

    home page and header and sponsor and about

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,10 +1,10 @@
-myApp.controller("headerCtrl", function (
+myApp.controller("headerCtrl", function(
   $scope,
   TemplateService,
   NavigationService
 ) {
   $scope.template = TemplateService;
-  $scope.$on("$stateChangeSuccess", function (
+  $scope.$on("$stateChangeSuccess", function(
     event,
     toState,
     toParams,
@@ -18,11 +18,11 @@ myApp.controller("headerCtrl", function (
   var categorySubtitle = [];
   $scope.adminurl = adminurl;
   // GET Team
-  $scope.getTeam = function () {
+  $scope.getTeam = function() {
     $scope.url = "Teamlist/getList";
     $scope.constraints = {};
-    NavigationService.apiCall($scope.url, $scope.constraints, function (data) {
-      _.each(data.data, function (n) {
+    NavigationService.apiCall($scope.url, $scope.constraints, function(data) {
+      _.each(data.data, function(n) {
         teamSubtitle.push({
           name: n.name,
           classis: "active",
@@ -36,11 +36,11 @@ myApp.controller("headerCtrl", function (
   // GET  Team END
 
   // GET Category
-  $scope.getCategory = function () {
+  $scope.getCategory = function() {
     $scope.url = "Categorylist/getList";
     $scope.constraints = {};
-    NavigationService.apiCall($scope.url, $scope.constraints, function (data) {
-      _.each(data.data, function (n) {
+    NavigationService.apiCall($scope.url, $scope.constraints, function(data) {
+      _.each(data.data, function(n) {
         categorySubtitle.push({
           name: n.name,
           classis: "active",
@@ -54,12 +54,14 @@ myApp.controller("headerCtrl", function (
   // GET  Category END
 
   if (_.isEmpty(teamSubtitle) && _.isEmpty(categorySubtitle)) {
-    $scope.headerMenu = [{
+    $scope.headerMenu = [
+      {
         name: "Home",
         classis: "active",
         anchor: "home",
         subnav: []
-    },{
+      },
+      {
         name: "Auction Stats",
         classis: "active",
         anchor: "auction",
@@ -77,18 +79,18 @@ myApp.controller("headerCtrl", function (
         anchor: "soldplayers",
         subnav: []
       },
-      // {
-      //   name: "Sponsor",
-      //   classis: "active",
-      //   anchor: "sponsor",
-      //   subnav: []
-      // },
-      // {
-      //   name: "About Us",
-      //   classis: "active",
-      //   anchor: "about",
-      //   subnav: []
-      // },
+      {
+        name: "Sponsor",
+        classis: "active",
+        anchor: "sponsor",
+        subnav: []
+      },
+      {
+        name: "About Us",
+        classis: "active",
+        anchor: "about",
+        subnav: []
+      },
       {
         name: "Live Stats",
         classis: "active",
