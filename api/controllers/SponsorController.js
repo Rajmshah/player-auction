39c81c4dda327243ddf3677598001d928547c19d
Rajmshah <rajwohlig@gmail.com>
2019-03-10 10:07:59 +0530

    done sponsor page

--- api/controllers/SponsorController.js
+++ api/controllers/SponsorController.js
@@ -1,7 +1,9 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
   getlist: function(req, res) {
-    Sponsor.find().exec(res.callback);
+    Sponsor.find({ active: true })
+      .sort({ order: 1 })
+      .exec(res.callback);
   }
 };
 module.exports = _.assign(module.exports, controller);
